4aa28d2b497462694e0147d411d9611dc9e69476
JinMo-source <qkrwslah123@naver.com>
2022-04-14 07:30:33 +0900

    first commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vw;
+  background: red;
+`;
+function Home() {
+  console.log();
+  return <Container>Home</Container>;
+}
+
+export default Home;
