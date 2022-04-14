20bdefe2a41608869d8c8f8874101eff0fb3237e
JinMo-source <qkrwslah123@naver.com>
2022-04-14 15:55:05 +0900

    Intro 사진제외 완성

---
+++ src/Menu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const MenuContainer = styled.menu`
+  width: 200px;
+  padding: 30px 30px;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  right: auto;
+  top: 0;
+  bottom: 0;
+  background: #181818;
+  color: #fff;
+`;
+function Menu() {
+  console.log();
+  return <MenuContainer>Menu</MenuContainer>;
+}
+
+export default Menu;
